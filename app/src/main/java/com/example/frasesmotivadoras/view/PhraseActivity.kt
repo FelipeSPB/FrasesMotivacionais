88a784931b338af57f0c700e118832dd3dae5434
FelipeSPB <felipebiancalana@outlook.com>
2020-11-05 20:04:14 -0300

    Share Button implementado

--- app/src/main/java/com/example/frasesmotivadoras/view/PhraseActivity.kt
+++ app/src/main/java/com/example/frasesmotivadoras/view/PhraseActivity.kt
@@ -1,15 +1,19 @@
 package com.example.frasesmotivadoras.view
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.frasesmotivadoras.R
 import com.example.frasesmotivadoras.model.Phrase
 import com.example.frasesmotivadoras.viewmodel.ViewModelPhrase
+import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.activity_phrase.*
 
 class PhraseActivity  : AppCompatActivity() {
     private lateinit var viewModelPhrase: ViewModelPhrase
@@ -17,7 +21,25 @@ class PhraseActivity  : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phrase)
         viewModelPhrase = ViewModelProviders.of(this).get(ViewModelPhrase::class.java)
-        viewModelPhrase.setCamps(phrase = findViewById<TextView>(R.id.phrase_famous),
-                thinker = findViewById<TextView>(R.id.writer_famous));}
+        val phrase = findViewById<TextView>(R.id.phrase_famous)
+        val thinker = findViewById<TextView>(R.id.writer_famous)
+        viewModelPhrase.setCamps(phrase, thinker);
+        button_share.setOnClickListener() {
+            val sharePhrase: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "${phrase.text} \n \n                       ${thinker.text} \n \n${getString(R.string.share_phrase)}")
+                type = "text/plain"
+
+            }
+            val shareIntent = Intent.createChooser(sharePhrase, "Compartilhe sua frase motivadora !")
+            startActivity(shareIntent)
+        }
+
+
+    }
+
+
 
 }
+
+
--- app/src/main/java/com/example/frasesmotivadoras/viewmodel/ViewModelPhrase.kt
+++ app/src/main/java/com/example/frasesmotivadoras/viewmodel/ViewModelPhrase.kt
@@ -2,14 +2,22 @@ package com.example.frasesmotivadoras.viewmodel
 
 import android.app.Application
 import android.content.Context
+import android.content.Intent
+import android.content.Intent.*
 import android.util.Log
+import android.view.Gravity.apply
+import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.view.GravityCompat.apply
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.example.frasesmotivadoras.model.JSONRepository
 import com.example.frasesmotivadoras.model.Phrase
+import com.google.android.material.button.MaterialButton
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import android.view.Gravity.apply
 import kotlin.random.Random.Default.nextInt
 
 class ViewModelPhrase(application: Application): AndroidViewModel(application) {
@@ -17,12 +25,18 @@ class ViewModelPhrase(application: Application): AndroidViewModel(application) {
     val context: Context = getApplication<Application>().applicationContext
     val repository: JSONRepository = JSONRepository(context)
 
-    fun setCamps(phrase: TextView, thinker: TextView){
+    fun setCamps(phrase: TextView, thinker: TextView) {
         var len = repository.phrases.size
-        var chosenPhrase = repository.phrases.get(nextInt(0,len))
+        var chosenPhrase = repository.phrases.get(nextInt(0, len))
         var phraseOfTheDay = chosenPhrase.phrase.toString()
         var thinkerOfTheDay = chosenPhrase.nameThinker.toString()
-        phrase.text=phraseOfTheDay
-        thinker.text=thinkerOfTheDay
-        }
+        phrase.text = phraseOfTheDay
+        thinker.text = thinkerOfTheDay
+    }
+
 }
+
+
+
+
+
